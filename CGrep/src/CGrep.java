8166cd6b264ae2c343df82d7a7c26a707f80da35
zps7119 <zps7119@WAYNE.se.rit.edu>
2012-03-30 15:09:46 -0400

    CGrep

--- CGrep/src/CGrep.java
+++ CGrep/src/CGrep.java
@@ -1,13 +1,26 @@
+import java.util.ArrayList;
+import java.util.concurrent.Future;
+
 
 public class CGrep {
 
 	/**
 	 * @param args
 	 */
+	
+	ArrayList<Future<Found>> futureList = new ArrayList<Future<Found>>();
+	
 	public static void main(String[] args) {
+		
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hello World");
+		
+		for(int i = 1; i < args.length-1; i++){
+			
+			futureList.add(executor.submit());
+			
+		}
+		
 		
 	}
 
