7f4f99c87e7b2d2e97073ccc4ad8a792ed4158bf
zps7119 <zps7119@WAYNE.se.rit.edu>
2012-03-30 15:23:47 -0400

    CGrep main process

--- CGrep/src/CGrep.java
+++ CGrep/src/CGrep.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 
@@ -8,17 +10,18 @@ public class CGrep {
 	 * @param args
 	 */
 	
-	ArrayList<Future<Found>> futureList = new ArrayList<Future<Found>>();
-	ExecutorService executor;
+	static ArrayList<Future<Found>> futureList = new ArrayList<Future<Found>>();
+	static ExecutorService executor;
 	
 	public static void main(String[] args) {
+		executor = Executors.newFixedThreadPool(3);
 		
 		// TODO Auto-generated method stub
 		
 		
 		for(int i = 1; i < args.length-1; i++){
 			
-			futureList.add(executor.submit());
+			futureList.add(executor.submit(new Grep(args[i], args[0])));
 			
 		}
 		
---
+++ CGrep/src/Grep.java
@@ -0,0 +1,22 @@
+import java.util.concurrent.Callable;
+import java.util.concurrent.Future;
+
+
+public class Grep implements Callable{
+
+
+
+	public Grep(String string, String string2) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Future call() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+
+
+}
